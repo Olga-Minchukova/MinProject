6e655e1df0978d8fd30bd1b9935f1627c5e7af1c
OLGA Minchukova <omin1110@gmail.com>
2023-08-16 01:06:50 +0300

    first commit

---
+++ MinProject/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, Нетология!");
+        System.out.println("2+3");
+        System.out.println(2 + 3);
+
+        int speed = 17;
+        int x=speed + 3;
+
+        String name = " Petya ";
+
+        System.out.println (name + " ехал со скоростью " + speed);
+
+    }
+}
