8c3a50256bc100bf923dd044eb003cde17d80552
OLGA Minchukova <omin1110@gmail.com>
2023-08-18 00:34:43 +0300

    MinProject/src/Main.java

--- MinProject/src/Main.java
+++ MinProject/src/Main.java
@@ -1,15 +1,19 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, Нетология!");
-        System.out.println("2+3");
-        System.out.println(2 + 3);
 
-        int speed = 17;
-        int x=speed + 3;
+        int eaters = 5; // сколько людей будут есть
 
-        String name = " Petya ";
+        int water = 3000; // миллилитров воды
+        int potatoes = 5; // картофелин
+        int chicken = 6; // куриных бёдер
+        int spices = 10; // ложек специй
 
-        System.out.println (name + " ехал со скоростью " + speed);
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println ((water / eaters) + " миллилитров воды");
+        System.out.println ((potatoes / eaters) + " картофелин(а/ы)");
+        System.out.println ((chicken / eaters) + " куриных(ое) бёдер(ро)");
+        System.out.println ((spices / eaters) + " ложек(ки/ка) специй");
 
     }
 }
